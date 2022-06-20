3505440ff0b8385cffceaa955a511a38657f74ce
unknown <kumarsinghaakash1@gmail.com>
2022-06-20 09:41:22 +0530

    program

---
+++ Javacode/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+
+        System.out.print("Enter height: ");
+        int h = s.nextInt();
+
+        System.out.print("Enter width: ");
+        int w = s.nextInt();
+
+        for (int i=1; i<=h; i++) {
+            for (int j=1; j<=w; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
